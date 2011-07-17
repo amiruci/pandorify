2326ac2cfca0d43ad8f2412b67c6f8cc6ada96ab
Amir Haghighat <amiruci@gmail.com>
2011-07-17 16:14:37 -0700

    moar shiz

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import os
 import urllib2
 
+from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import template
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -18,11 +19,39 @@ class MainPage(webapp.RequestHandler):
 
 
 class Pandorify(webapp.RequestHandler):
+    
+    spotify_song_query_template = 'http://ws.spotify.com/search/1/track.json?q=%(artist)s %(title)s'
+    
     def post(self):
         url = self.request.get('url').strip()
         usock = urllib2.urlopen(url) 
         xmldoc = minidom.parse(usock) 
         usock.close()
+        songs = self.getSongsFromXmlDocument(xmldoc)
+        for song in songs:
+            spotify_song_query = Pandorify.spotify_song_query_template % {'artist': song.artist, 'title': song.title}
+            try:
+                spotify_query_result = urlfetch.fetch(spotify_song_query, method=urlfetch.GET, deadline=10)
+            except urllib2.URLError, e:
+                logging.error(e)
+                self.response.out.write('%(error_code)s for %(query)s :(<hr>' % {'error_code': spotify_query_result.status_code, 'query': spotify_song_query})
+                continue
+            if spotify_query_result.status_code != 200:
+                self.response.out.write('%(error_code)s for %(query)s :(<hr>' % {'error_code': spotify_query_result.status_code, 'query': spotify_song_query})
+                continue
+        
+            json_result = json.loads(spotify_query_result.content)
+            tracks = json_result.get('tracks', {})
+            if tracks:
+                # Select the first track for now
+                # TODO(amir): be smart about which track you select
+                track = tracks[0]
+                self.response.out.write('<a href="%(url)s">%(title)s by %(artist)s</a> for query "%(query)s"<hr>' %
+                                        {'url': track['href'], 'title':  track['name'], 'artist': track['artists'][0]['name'], 'query': song.artist + ' ' + song.title})
+            else:
+                self.response.out.write('Nothing found for "%s" :(<hr>' % spotify_song_query)
+
+    def getSongsFromXmlDocument(self, xmldoc):
         # <rss xmlns:pandora="http://www.pandora.com/rss/1.0/modules/pandora/" 
         # xmlns:mm="http://musicbrainz.org/mm/mm-2.1#" version="2.0">
         pandora_ns_uri = xmldoc.childNodes[0].attributes['xmlns:pandora'].nodeValue
@@ -41,47 +70,14 @@ class Pandorify(webapp.RequestHandler):
             artist = item.getElementsByTagNameNS(mm_ns_uri, 'Artist')[0]
             artist_title = artist.getElementsByTagNameNS(dc_ns_uri, 'title')[0].firstChild.data
             url = item.getElementsByTagNameNS(pandora_ns_uri, 'albumArtUrl')[0].firstChild.data
-            song = Song(track_title, artist_title, url)
-            songs.append(song)
-
-        # Just query for 3 songs to avoid 503s :/
-        songs = songs[:3]
-        for song in songs:
-            spotify_query = 'http://ws.spotify.com/search/1/track.json?q=' + song.artist + ' ' + song.title
-            spotify_query_result = urllib2.urlopen(spotify_query)
-            json_result = json.loads(spotify_query_result.read())
-            tracks = json_result.get('tracks', {})
-            if not tracks:
-                self.response.out.write('Nothing found for "'+spotify_query+'" :(')
-                continue
-            self.response.out.write('<a href="'+tracks[0]['href']+'">' + tracks[0]['name'] + ' by ' + tracks[0]['artists'][0]['name'] + '</a> for query "'+ song.artist + ' ' + song.title+'"<hr>')
-
-
-class SpotifySearcher(webapp.RequestHandler):
-    def get(self):
-        try:
-            query = self.request.get('q').strip()
-            result = urllib2.urlopen('http://ws.spotify.com/search/1/track.json?q='+query)
-            json_result = json.loads(result.read())
-            tracks = json_result.get('tracks', {})
-            if not tracks:
-                self.response.out.write('Nothing found for "'+query+'" :(')
-                return
-            self.response.out.write('Showing results for "'+query+'"<br><br>')
-            for track in tracks:
-                self.response.out.write('<a href="'+track['href']+'">' + track['name'] + ' by ' + track['artists'][0]['name'] + '</a><hr>')
-            
-        except urllib2.URLError, e:
-            logging.error(e)
-            self.response.out.write(e)
+            songs.append(Song(track_title, artist_title, url))
+        return songs
 
 
 application = webapp.WSGIApplication(
                                     [('/', MainPage),
-                                     ('/search', SpotifySearcher),
                                      ('/pandorify', Pandorify)],
                                      debug=True)
-
 
 
 def main():
